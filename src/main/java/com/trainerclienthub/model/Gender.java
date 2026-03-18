6a485d64c4e0b78402e6bc7b7fe91589a41ec670
Smaran <smaranaryal60@gmail.com>
2026-03-18 21:48:26 +0545

    gender

---
+++ src/main/java/com/trainerclienthub/model/Gender.java
@@ -0,0 +1,11 @@
+package com.trainerclienthub.model;
+
+/**
+ * Represents the gender options available for a {@link Client}.
+ * Mirrors the ENUM constraint defined in the {@code client} database table.
+ */
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
