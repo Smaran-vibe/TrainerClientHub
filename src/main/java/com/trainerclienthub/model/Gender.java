ee4b70ceea07da27afd5c41efad75f963aa302c8
Smaran <smaranaryal60@gmail.com>
2026-03-23 21:15:11 +0545

    fixed minor error

--- src/main/java/com/trainerclienthub/model/Gender.java
+++ src/main/java/com/trainerclienthub/model/Gender.java
@@ -1,9 +1,6 @@
 package com.trainerclienthub.model;
 
-/**
- * Represents the gender options available for a {@link Client}.
- * Mirrors the ENUM constraint defined in the {@code client} database table.
- */
+
 public enum Gender {
     MALE,
     FEMALE,
